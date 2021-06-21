8eccd88c5731063c7ae7e73877e488530ca41224
ni <sbgs.nidhi.prakash@gmail.com>
2021-06-21 21:09:25 +0530

    mod

---
+++ precache-manifest.a6b58e94f2fa54029f8eacb633d3bb41.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a9945f116bad981b14be3740e97c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7541db16f067a13328ee",
+    "url": "/static/css/main.176c70e2.chunk.css"
+  },
+  {
+    "revision": "7c51aae935e5f48f5a7f",
+    "url": "/static/js/2.88219d88.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.88219d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7541db16f067a13328ee",
+    "url": "/static/js/main.790f1e17.chunk.js"
+  },
+  {
+    "revision": "40f4048ae285fb4faa2d",
+    "url": "/static/js/runtime-main.f37f0e70.js"
+  },
+  {
+    "revision": "dbc191458a25aa205a7c0f034604e7ea",
+    "url": "/static/media/header.dbc19145.jpg"
+  },
+  {
+    "revision": "2a587a104b4dce5f9308f3e906ba4c13",
+    "url": "/static/media/kaksha.2a587a10.jpeg"
+  },
+  {
+    "revision": "2f18e487560e8ef3d46682bbfc35efd7",
+    "url": "/static/media/me.2f18e487.jpeg"
+  },
+  {
+    "revision": "63d6ed3987c20da095af0f225adf1663",
+    "url": "/static/media/parallax1.63d6ed39.jpg"
+  },
+  {
+    "revision": "cec8f460c4dbeb70bd3c8b081fa81014",
+    "url": "/static/media/parallax2.cec8f460.jpg"
+  },
+  {
+    "revision": "601c6f4cd0d6e256efd1f65049400c74",
+    "url": "/static/media/project1.601c6f4c.jpeg"
+  },
+  {
+    "revision": "ca301c213e84ba0b91e47f3c994569ff",
+    "url": "/static/media/project2.ca301c21.jpg"
+  }
+]);
